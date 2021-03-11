8b76ca949f7a10c021e2bdedec5b10afff82943f
audrey <theninja835@gmail.com>
2021-03-10 19:32:27 -0600

    Registered the Event Handler

--- src/main/java/com/gmail/kurumitk78/unify/Unify.java
+++ src/main/java/com/gmail/kurumitk78/unify/Unify.java
@@ -1,17 +1,14 @@
 package com.gmail.kurumitk78.unify;
 
+import com.gmail.kurumitk78.unify.events.OnBlockInteract;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Unify extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new OnBlockInteract(), this);
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
