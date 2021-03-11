405a90d9a4744152a3e5691cd632e47274f7137b
audrey <theninja835@gmail.com>
2021-03-11 14:57:29 -0600

    Adds Netherite to list of detected items if server version is detected as 1.16.X

--- src/main/java/com/gmail/kurumitk78/unify/Unify.java
+++ src/main/java/com/gmail/kurumitk78/unify/Unify.java
@@ -1,12 +1,14 @@
 package com.gmail.kurumitk78.unify;
 
 import com.gmail.kurumitk78.unify.events.OnBlockInteract;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Unify extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if(Bukkit.getServer().getVersion().contains("MC: 1.16")){ OnBlockInteract.addNewMaterials();}
         getServer().getPluginManager().registerEvents(new OnBlockInteract(), this);
 
     }
