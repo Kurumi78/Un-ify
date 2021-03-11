ce5bf4cc68b5890fa3102c97458685e7195558c7
audrey <theninja835@gmail.com>
2021-03-10 19:32:43 -0600

    Made the code actually work

--- src/main/java/com/gmail/kurumitk78/unify/events/OnBlockInteract.java
+++ src/main/java/com/gmail/kurumitk78/unify/events/OnBlockInteract.java
@@ -1,5 +1,6 @@
 package com.gmail.kurumitk78.unify.events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -28,26 +29,26 @@ public class OnBlockInteract implements Listener {
 
     @EventHandler
     public void OnBlockRightClick(PlayerInteractEvent event){
-
-        if(event.getMaterial().equals(Material.GRASS_PATH) &&  shovelArray.contains(event.getPlayer().getEquipment().getItemInMainHand().getType())){ //Unpathing
+        //event.getPlayer().sendMessage(ChatColor.GRAY + "[Un-Ify] " + ChatColor.DARK_GRAY + event.getMaterial() + " " + shovelArray.contains(event.getPlayer().getEquipment().getItemInMainHand().getType()));
+        if(event.getClickedBlock().getType().equals(Material.GRASS_PATH) &&  shovelArray.contains(event.getPlayer().getEquipment().getItemInMainHand().getType())){ //Unpathing
             Location pathLoc = event.getClickedBlock().getLocation();
             World world = event.getClickedBlock().getWorld();
-            world.getBlockAt(pathLoc).setType(Material.GRASS);
+            world.getBlockAt(pathLoc).setType(Material.GRASS_BLOCK);
             ItemStack shovel = event.getPlayer().getEquipment().getItemInMainHand();
             Damageable shovelMeta = (Damageable) shovel.getItemMeta();
-            shovelMeta.setDamage(shovelMeta.getDamage()-1);
+            shovelMeta.setDamage(shovelMeta.getDamage()+1);
             shovel.setItemMeta((ItemMeta)shovelMeta);
 
 
         }
 
-        else if(event.getMaterial().equals(Material.FARMLAND) &&  hoeArray.contains(event.getPlayer().getEquipment().getItemInMainHand().getType())){
+        else if(event.getClickedBlock().getType().equals(Material.FARMLAND) &&  hoeArray.contains(event.getPlayer().getEquipment().getItemInMainHand().getType())){
             Location tilledLoc = event.getClickedBlock().getLocation();
             World world = event.getClickedBlock().getWorld();
             world.getBlockAt(tilledLoc).setType(Material.DIRT);
             ItemStack hoe = event.getPlayer().getEquipment().getItemInMainHand();
             Damageable hoeMeta = (Damageable) hoe.getItemMeta();
-            hoeMeta.setDamage(hoeMeta.getDamage()-1);
+            hoeMeta.setDamage(hoeMeta.getDamage()+1);
             hoe.setItemMeta((ItemMeta)hoeMeta);
 
         }
