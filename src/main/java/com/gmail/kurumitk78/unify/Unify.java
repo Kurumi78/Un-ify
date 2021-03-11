40ce21af9b1903fb76b3e81a00992774492782b7
audrey <theninja835@gmail.com>
2021-03-11 15:42:14 -0600

    Added BStats Metrics

--- src/main/java/com/gmail/kurumitk78/unify/Unify.java
+++ src/main/java/com/gmail/kurumitk78/unify/Unify.java
@@ -3,6 +3,7 @@ package com.gmail.kurumitk78.unify;
 import com.gmail.kurumitk78.unify.events.OnBlockInteract;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bstats.bukkit.Metrics;
 
 public final class Unify extends JavaPlugin {
 
@@ -10,6 +11,10 @@ public final class Unify extends JavaPlugin {
     public void onEnable() {
         if(Bukkit.getServer().getVersion().contains("MC: 1.16")){ OnBlockInteract.addNewMaterials();}
         getServer().getPluginManager().registerEvents(new OnBlockInteract(), this);
+
+
+        int pluginId = 10628;
+        Metrics metrics = new Metrics(this, pluginId);
 
     }
 
