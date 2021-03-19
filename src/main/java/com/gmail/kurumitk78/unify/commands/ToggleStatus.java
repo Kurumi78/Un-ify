ea67ccedf9ca39929f2b9ad3eac469e1008b87d8
audrey <theninja835@gmail.com>
2021-03-19 17:45:02 -0500

    I really dont like how this is formatted, but TO BAD. this command toggles if the user has the plugin enabled or not

---
+++ src/main/java/com/gmail/kurumitk78/unify/commands/ToggleStatus.java
@@ -0,0 +1,16 @@
+package com.gmail.kurumitk78.unify.commands;
+
+import com.gmail.kurumitk78.unify.Unify;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ToggleStatus implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!Unify.enabledPeople.contains(sender.getName())){
+            Unify.enabledPeople.add(sender.getName());
+        } else { Unify.enabledPeople.remove(sender.getName());}
+        return false;
+    }
+}
