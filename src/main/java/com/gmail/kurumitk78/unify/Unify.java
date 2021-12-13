020e2443aa1d9374a1362fb821eecc60593cf9a8
Kurumi78 <Kurumitk78@gmail.com>
2021-12-12 19:26:57 -0600

    OnEnable changes for the new Unstripping Feature

--- src/main/java/com/gmail/kurumitk78/unify/Unify.java
+++ src/main/java/com/gmail/kurumitk78/unify/Unify.java
@@ -1,6 +1,7 @@
 package com.gmail.kurumitk78.unify;
 
 import com.gmail.kurumitk78.unify.commands.ToggleStatus;
+import com.gmail.kurumitk78.unify.events.OnAxeUse;
 import com.gmail.kurumitk78.unify.events.OnHoeUse;
 import com.gmail.kurumitk78.unify.events.OnShovelUse;
 import org.bukkit.Bukkit;
@@ -22,8 +23,11 @@ public final class Unify extends JavaPlugin {
         if (!(new File(this.getDataFolder(), "config.yml").exists())) { // Generates the config if missing,
             this.saveDefaultConfig();
         }
-        if(Bukkit.getServer().getVersion().contains("MC: 1.16")){ OnHoeUse.addNewMaterials(); //adds netherite tools if 1.16 is detected
-            OnShovelUse.addNewMaterials();}
+        if(Bukkit.getServer().getVersion().contains("MC: 1.16")){
+            OnHoeUse.addNewMaterials(); //adds netherite tools if 1.16 is detected
+            OnShovelUse.addNewMaterials();
+            OnAxeUse.addNewMaterials();
+        }
 
         if(this.getConfig().getBoolean("Unpathing")) { //Enables unpathing if enabled via config
             getServer().getPluginManager().registerEvents(new OnShovelUse(), this);
@@ -31,6 +35,10 @@ public final class Unify extends JavaPlugin {
 
         if(this.getConfig().getBoolean("Untilling")) { //Enables untilling if enabled via config
             getServer().getPluginManager().registerEvents(new OnHoeUse(), this);
+        }
+
+        if(this.getConfig().getBoolean("Unstripping")) { //Enables untilling if enabled via config
+            getServer().getPluginManager().registerEvents(new OnAxeUse(), this);
         }
 
         enabledPeople = this.getConfig().getStringList("EnabledUsers");    //Sets up the list of valid people on startup from config
