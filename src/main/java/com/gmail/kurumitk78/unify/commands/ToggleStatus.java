f66db8e250df11a685ac5be6c27df249e3c59bc2
Kurumi78 <Kurumitk78@gmail.com>
2021-12-12 19:42:20 -0600

    Added a basic message when enabling or disabling undoing mode

--- src/main/java/com/gmail/kurumitk78/unify/commands/ToggleStatus.java
+++ src/main/java/com/gmail/kurumitk78/unify/commands/ToggleStatus.java
@@ -1,6 +1,7 @@
 package com.gmail.kurumitk78.unify.commands;
 
 import com.gmail.kurumitk78.unify.Unify;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,7 +12,11 @@ public class ToggleStatus implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!Unify.enabledPeople.contains(sender.getName())){
             Unify.enabledPeople.add(sender.getName());
-        } else { Unify.enabledPeople.remove(sender.getName());}
+            sender.sendMessage("&7[&8Un-ify&7]" + ChatColor.GRAY + " Un-doing enabled!");
+        } else {
+            Unify.enabledPeople.remove(sender.getName());
+            sender.sendMessage("&7[&8Un-ify7&]" + ChatColor.GRAY + " Un-doing disabled!");
+        }
         return false;
     }
 }
